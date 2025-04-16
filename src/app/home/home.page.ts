1276aa1f67bd5e9c0dfb0bfa70c7b643edbc6c07
unknown <g00427804@atu.ie>
2025-04-16 10:49:02 +0100

    Added a user login functionality on the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone'; // importing IonButton
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone'; // importing IonButton
 import { RouterModule } from '@angular/router'; // importing router
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, FormsModule, IonInput, IonLabel, IonItem, RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
+
 export class HomePage {
+  // variables
+  username: string = "";
+
   constructor() {}
+
+  saveUser()
+  {
+    localStorage.setItem("username", this.username);
+    //console.log(this.username); DEBUG
+  } // saveUser
 }
