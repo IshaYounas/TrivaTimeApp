a1d682889e05d88bf4e5bcabc339fee02cdbe52b
unknown <g00427804@atu.ie>
2025-04-18 09:52:48 +0100

    Added a fun fact api in the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput, IonIcon } from '@ionic/angular/standalone'; // importing IonButton
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone'; // importing IonButton
 import { RouterModule } from '@angular/router'; // importing router
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonIcon, CommonModule, FormsModule, IonInput, IonLabel, IonItem, RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule, FormsModule, IonInput, IonLabel, IonItem, RouterModule, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 
-export class HomePage {
+export class HomePage implements OnInit {
   // variables
   username: string = "";
+  funfact: string = "";
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit()
+  {
+    this.getFunFact();
+  }
 
+  getFunFact()
+  {
+    this.httpClient.get('https://uselessfacts.jsph.pl/random.json?language=en').subscribe(
+      (response: any) => {
+        this.funfact = response.text;
+      }
+    )
+  } // getFunFact
+  
   saveUser()
   {
     localStorage.setItem("username", this.username);
